a92fe3282f7281f4b31e5a8e4fe89542c34bd8c7
0xWares <md.abdulwares@outlook.com>
2026-04-24 08:10:54 +0600

    malfunctioning fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import Home from './components/pages/Home'
 // import Contact from './components/pages/Contact'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import CaffeinaJournal from './components/pages/CaffeinaJournal'
 import Contents from './components/Contents'
 import CulturalEssays from './components/navComponents/CulturalEssays'
@@ -12,11 +12,11 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<CaffeinaJournal/>}>
-          <Route index element={<CulturalEssays/>}/>
-          <Route path='culturalessays' element={<CulturalEssays/>}/>
-          <Route path='fieldstories' element={<FieldStories/>}/>
-          <Route path='researchpapers' element={<ResearchPapers/>}/>
+        <Route path='/' element={<CaffeinaJournal />}>
+          <Route index element={<Navigate to="researchpapers" replace />} />
+          <Route path='culturalessays' element={<CulturalEssays />} />
+          <Route path='fieldstories' element={<FieldStories />} />
+          <Route path='researchpapers' element={<ResearchPapers />} />
         </Route>
       </Routes>
     </div>
